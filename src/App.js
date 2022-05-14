dc4003f22ac2a9b318588156b8e4d58b4ce3061c
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2022-05-14 16:59:51 +0530

    add main pagination component to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Pagination from "./components/pagination";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Pagination itemsPerPage={20} />
+    </div>
+  );
 }
 
 export default App;
